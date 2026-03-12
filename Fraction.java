d2091791aaeee550e7113d43fde39424a036f2bf
Arrow-Ace1 <murphdre000@rocori.k12.mn.us>
2026-03-12 16:26:45 +0000

    dw

--- Fraction.java
+++ Fraction.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class debug {
+public class Fraction {
     public static void main (String[) args) {
         Scanner scanner = new Scanner (System. in) ;
         int numerator, denominator, result;
