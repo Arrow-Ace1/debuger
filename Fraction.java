87de36ee811fc8333a20737b4eaa7d30ffcb6df9
Arrow-Ace1 <murphdre000@rocori.k12.mn.us>
2026-03-12 16:36:46 +0000

    fvfv

--- Fraction.java
+++ Fraction.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Fraction {
-    public static void main (String[) args) {
-        Scanner scanner = new Scanner (System. in) ;
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
         int numerator, denominator, result;
 
-        System.out.print ("Enter Numerator: "):
-        numerator = scanner.nextInt ();
+        System.out.print ("Enter Numerator: ");
+        numerator = scanner.nextInt();
+        
         System.out.print ("Enter Denominator: ");
-        denominator = scanner.nextInt ():
-        System.out.println ();
-        result = numerator / denominator:
+        denominator = scanner.nextInt();
+        
+        System.out.println();
+
+        // Perform integer division
+        result = numerator / denominator;
+
         System.out.println (numerator + "/" + denominator + " = " + result);
         scanner.close();
     }
